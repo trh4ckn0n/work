cb74d61f385ab79a959d6d80781b90db84e776de
trh4ckn0n <trhacknon@proton.me>
2025-03-19 09:57:16 +0100

    Update trknxs.js

--- trknxs.js
+++ trknxs.js
@@ -59,19 +59,20 @@
     // Titre de la page
     const title = document.createElement('h2');
     title.innerHTML = "🔌 XSS by Trhacknon 🔌";
-    document.body.appendChild(title);
     // Ajouter les logos SVG
-    const logoContainer = document.createElement('div');
-    logoContainer.classList.add('logo');
-    logoContainer.innerHTML = `
-        <center><img src="https://github.com/trh4ckn0n/work/raw/refs/heads/main/trknanon.svg" alt="Logo 1" /></center><br>
-        <center><img src="https://raw.githubusercontent.com/trh4ckn0n/work/refs/heads/main/trkncat.svg" alt="Logo 2" /></center>
-    `;
     container.appendChild(logoContainer);
     // Conteneur pour afficher les données
     const container = document.createElement('div');
     container.classList.add('container');
     document.body.appendChild(container);
+    const logoContainer = document.createElement('div');
+    logoContainer.classList.add('logo');
+    document.body.appendChild(title);
+    logoContainer.innerHTML = `
+        <center><img src="https://github.com/trh4ckn0n/work/raw/refs/heads/main/trknanon.svg" alt="Logo 1" /></center><br>
+        <center><img src="https://raw.githubusercontent.com/trh4ckn0n/work/refs/heads/main/trkncat.svg" alt="Logo 2" /></center>
+    `;
+
 
 
     // Table pour afficher les informations collectées
